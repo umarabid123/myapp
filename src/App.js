a64c85370d84906910a45f129c8843ca427fac63
umarabid123 <umarabid709@gmail.com>
2024-06-16 12:19:02 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [color, setColor] = useState("olive");
+
   return (
-    <div className="App">
-     <h1 className='text-left'>React App</h1>
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center bg-white gap-3 px-3 py-2 shadow-lg rounded-3xl">
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "red"}}onClick={() => setColor("red")}>Red</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "blue"}}onClick={() => setColor("blue")}>Blue</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "green"}} onClick={() => setColor("green")}>Green</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "black"}} onClick={() => setColor("black")}>Black</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "cyan"}}  onClick={() => setColor("cyan")}>Cyan</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "pink"}} onClick={() => setColor("pink")}>Pink</button>
+          <button className="outline-none px-4 py-1 rounded-full text-white shadow-lg" style={{backgroundColor: "gray"}} onClick={() => setColor("gray")}>Gray</button>
+       
+        </div>
+      </div>
     </div>
   );
 }
